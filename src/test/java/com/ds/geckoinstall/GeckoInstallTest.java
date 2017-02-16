98b2e7df8553a2d79c02be08c74bfdf87eb828ae
Oscar <oquintela@altimetrik.com>
2017-02-16 12:21:22 -0300

    Update macosx dependencies and selenium 3.0

--- src/test/java/com/ds/geckoinstall/GeckoInstallTest.java
+++ src/test/java/com/ds/geckoinstall/GeckoInstallTest.java
@@ -5,16 +5,21 @@ package com.ds.geckoinstall;
 import java.io.File;
 import com.ds.geckoinstall.GeckoInstall;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 
 public class GeckoInstallTest {
+	
+	WebDriver driver;
 
 	@Test
-	public void testSeleniumGhostDriver() {
+	public void testGeckoInstaller() {
 		File geckoInstall = GeckoInstall.unpack();
-		//DesiredCapabilities dcaps = new DesiredCapabilities();
-		//System.setProperty("webdriver.gecko.driver", "/Applications/geckodriver");	
-		System.out.println(geckoInstall.getAbsolutePath());
+		System.setProperty("webdriver.gecko.driver", geckoInstall.getAbsolutePath());	
+		this.driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+	
 	}
 }
